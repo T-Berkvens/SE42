ed7ddc2654d9337c4cd5c595748faf8e561917e2
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-13 14:20:25 +0200

    updated tests added an sqlconnector

--- auction/src/test/java/auction/service/JPARegistrationMgrTest.java
+++ auction/src/test/java/auction/service/JPARegistrationMgrTest.java
@@ -8,14 +8,22 @@ import org.junit.Before;
 import org.junit.Test;
 
 import auction.domain.User;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import util.DatabaseCleaner;
 
 public class JPARegistrationMgrTest {
 
     private RegistrationMgr registrationMgr;
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("auctionPU");
+    private EntityManager em = emf.createEntityManager();
+    private DatabaseCleaner cleaner = new DatabaseCleaner(em);
 
     @Before
     public void setUp() throws Exception {
         registrationMgr = new RegistrationMgr();
+        cleaner.clean();
     }
 
     @Test
@@ -25,7 +33,7 @@ public class JPARegistrationMgrTest {
         User user2 = registrationMgr.registerUser("xxx2@yyy2");
         assertTrue(user2.getEmail().equals("xxx2@yyy2"));
         User user2bis = registrationMgr.registerUser("xxx2@yyy2");
-        assertSame(user2bis, user2);
+        assertEquals(user2bis, user2);
         //geen @ in het adres
         assertNull(registrationMgr.registerUser("abc"));
     }
@@ -34,7 +42,7 @@ public class JPARegistrationMgrTest {
     public void getUser() {
         User user1 = registrationMgr.registerUser("xxx5@yyy5");
         User userGet = registrationMgr.getUser("xxx5@yyy5");
-        assertSame(userGet, user1);
+        assertEquals(userGet, user1);
         assertNull(registrationMgr.getUser("aaa4@bb5"));
         registrationMgr.registerUser("abc");
         assertNull(registrationMgr.getUser("abc"));
