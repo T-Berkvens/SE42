88719fca43f8356dea3d7d26b7a3004b6c30f700
Twan Berkvens <twan_Berkvens@live.nl>
2017-06-13 19:51:07 +0200

    auction methodes ingevuld

--- auction/src/main/java/auction/service/AuctionMgr.java
+++ auction/src/main/java/auction/service/AuctionMgr.java
@@ -1,22 +1,44 @@
 package auction.service;
 
+import auction.dao.ItemDAO;
+import auction.dao.ItemDAOJPAImpl;
+import auction.dao.UserDAO;
+import auction.dao.UserDAOJPAImpl;
 import nl.fontys.util.Money;
 import auction.domain.Bid;
 import auction.domain.Item;
 import auction.domain.User;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class AuctionMgr  {
-
+    
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("auctionPU");
    /**
      * @param id
      * @return het item met deze id; als dit item niet bekend is wordt er null
      *         geretourneerd
      */
     public Item getItem(Long id) {
-        Item item = new Item();
-        return null;
+        EntityManager em = emf.createEntityManager();
+        ItemDAO itemDAO = new ItemDAOJPAImpl(em);
+        em.getTransaction().begin();
+        Item item = null;
+        try {
+            item = itemDAO.find(id);
+            em.getTransaction().commit();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+        }
+        finally {
+            em.close();
+        }
+        return item;
     }
 
   
@@ -25,8 +47,28 @@ public class AuctionMgr  {
      * @return een lijst met items met @desciption. Eventueel lege lijst.
      */
     public List<Item> findItemByDescription(String description) {
-        // TODO
-        return new ArrayList<Item>();
+        EntityManager em = emf.createEntityManager();
+        ItemDAO itemDAO = new ItemDAOJPAImpl(em);
+        em.getTransaction().begin();
+        ArrayList<Item> itemlst = new ArrayList<Item>();
+        List<Item> items = null;
+        try {
+            items = itemDAO.findByDescription(description);
+            em.getTransaction().commit();
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+            em.getTransaction().rollback();
+        }
+        finally {
+            em.close();
+        }
+        if (items != null) {
+            for (Item i : items) {
+                itemlst.add(i);
+            }
+        }
+        return itemlst;
     }
 
     /**
@@ -37,7 +79,25 @@ public class AuctionMgr  {
      *         amount niet hoger was dan het laatste bod, dan null
      */
     public Bid newBid(Item item, User buyer, Money amount) {
-        // TODO 
-        return null;
+        EntityManager em = emf.createEntityManager();
+        ItemDAO itemDAO = new ItemDAOJPAImpl(em);
+        em.getTransaction().begin();
+        Bid bid = null;
+        bid = item.newBid(buyer, amount);
+            if (bid != null) {
+            try {
+                itemDAO.edit(item);
+                em.getTransaction().commit();
+            }
+            catch(Exception ex) {
+                ex.printStackTrace();
+                em.getTransaction().rollback();
+                bid = null;
+            }
+            finally {
+                em.close();
+            }
+        }
+        return bid;
     }
 }
