cd294cef236d8f03d73d55a39710063f1cfa223f
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-13 13:40:30 +0200

    anotated USer

--- auction/src/main/java/auction/domain/User.java
+++ auction/src/main/java/auction/domain/User.java
@@ -1,8 +1,16 @@
 package auction.domain;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+    @Id
+    private Long id;
+    
     private String email;
+    
+    public User(){}
 
     public User(String email) {
         this.email = email;
@@ -11,5 +19,17 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
