e199ee3a7a240f9f5befaabc2099844592d4bdba
Twan Berkvens <twan_Berkvens@live.nl>
2017-06-14 20:15:42 +0200

    unittest fixed

--- auction/src/test/java/auction/service/ItemsFromSellerTest.java
+++ auction/src/test/java/auction/service/ItemsFromSellerTest.java
@@ -7,6 +7,7 @@ import auction.domain.Category;
 import auction.domain.Item;
 import auction.domain.User;
 import java.util.Iterator;
+import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -39,15 +40,15 @@ public class ItemsFromSellerTest {
         String omsch2 = "omsch_ifu2";
 
         User user1 = registrationMgr.registerUser(email);
-        assertEquals(0, user1.numberOfOfferdItems());
+        assertEquals(0, user1.numberOfOfferedItems());
 
         Category cat = new Category("cat2");
         Item item1 = sellerMgr.offerItem(user1, cat, omsch1);
 
        
         // test number of items belonging to user1
-        assertEquals(0, user1.numberOfOfferdItems());
-        assertEquals(1, user1.numberOfOfferdItems());
+        assertEquals(0, user1.numberOfOfferedItems());
+        assertEquals(1, user1.numberOfOfferedItems());
         
         /*
          *  expected: which one of te above two assertions do you expect to be true?
@@ -56,20 +57,20 @@ public class ItemsFromSellerTest {
          */
          
          
-        assertEquals(1, item1.getSeller().numberOfOfferdItems());
+        assertEquals(1, item1.getSeller().numberOfOfferedItems());
 
 
         User user2 = registrationMgr.getUser(email);
-        assertEquals(1, user2.numberOfOfferdItems());
+        assertEquals(1, user2.numberOfOfferedItems());
         Item item2 = sellerMgr.offerItem(user2, cat, omsch2);
-        assertEquals(2, user2.numberOfOfferdItems());
+        assertEquals(2, user2.numberOfOfferedItems());
 
         User user3 = registrationMgr.getUser(email);
-        assertEquals(2, user3.numberOfOfferdItems());
+        assertEquals(2, user3.numberOfOfferedItems());
 
         User userWithItem = item2.getSeller();
-        assertEquals(2, userWithItem.numberOfOfferdItems());
-        assertEquals(3, userWithItem.numberOfOfferdItems());
+        assertEquals(2, userWithItem.numberOfOfferedItems());
+        assertEquals(3, userWithItem.numberOfOfferedItems());
         /*
          *  expected: which one of te above two assertions do you expect to be true?
          *  QUESTION:
@@ -93,34 +94,27 @@ public class ItemsFromSellerTest {
 
         User user10 = registrationMgr.registerUser(email);
         Item item10 = sellerMgr.offerItem(user10, cat, omsch1);
-        Iterator<Item> it = user10.getOfferedItems();
+        Set<Item> it = user10.getOfferedItems();
         // testing number of items of java object
-        assertTrue(it.hasNext());
+        assertTrue(it.size() > 0);
         
         // now testing number of items for same user fetched from db.
         User user11 = registrationMgr.getUser(email);
-        Iterator<Item> it11 = user11.getOfferedItems();
-        assertTrue(it11.hasNext());
-        it11.next();
-        assertFalse(it11.hasNext());
+        Set<Item> it11 = user11.getOfferedItems();
+        assertTrue(it11.size() > 0);
+        assertFalse(it11.size() > 1);
 
         // Explain difference in above two tests for te iterator of 'same' user
 
-        
-        
         User user20 = registrationMgr.getUser(email);
         Item item20 = sellerMgr.offerItem(user20, cat, omsch2);
-        Iterator<Item> it20 = user20.getOfferedItems();
-        assertTrue(it20.hasNext());
-        it20.next();
-        assertTrue(it20.hasNext());
+        Set<Item> it20 = user20.getOfferedItems();
+        assertTrue(it20.size() > 1);
 
 
         User user30 = item20.getSeller();
-        Iterator<Item> it30 = user30.getOfferedItems();
-        assertTrue(it30.hasNext());
-        it30.next();
-        assertTrue(it30.hasNext());
+        Set<Item> it30 = user30.getOfferedItems();
+        assertTrue(it30.size() > 1);
 
     }
 }
