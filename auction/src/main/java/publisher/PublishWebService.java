e1eac05a20e8f6ef4e14dff5135d9a56577c9dc3
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-17 11:31:40 +0200

    completed the PublishWebService

--- auction/src/main/java/publisher/PublishWebService.java
+++ auction/src/main/java/publisher/PublishWebService.java
@@ -19,7 +19,7 @@ public class PublishWebService {
     
 
     public static void main(String[] args) {
-        Endpoint.publish(urlAuction, new Auction());
+        //Endpoint.publish(urlAuction, new Auction());
         Endpoint.publish(urlRegistration, new Registration());
     }
 }
