b29784100c99618263ee720234c386d2771a3170
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-17 12:54:42 +0200

    updated category added setter description

--- auction/src/main/java/auction/domain/Category.java
+++ auction/src/main/java/auction/domain/Category.java
@@ -19,4 +19,8 @@ public class Category implements Serializable {
     public String getDiscription() {
         return description;
     }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
