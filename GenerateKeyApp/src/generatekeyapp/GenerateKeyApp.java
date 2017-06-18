db9313cf4e6ab3ba9c99e6127d3dc2c0645c21f1
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-18 15:28:09 +0200

    added the generation of the keys

--- GenerateKeyApp/src/generatekeyapp/GenerateKeyApp.java
+++ GenerateKeyApp/src/generatekeyapp/GenerateKeyApp.java
@@ -5,17 +5,43 @@
  */
 package generatekeyapp;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Arno
  */
 public class GenerateKeyApp {
-
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+    
+    public GenerateKeyApp(){
+        GenerateKeys(1024);
+    }
+    
+    private void GenerateKeys(int length){
+        KeyPairGenerator keyGen = null;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(GenerateKeyApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        keyGen.initialize(length);
+        KeyPair keys = keyGen.genKeyPair();
+        PublicKey publicKey = keys.getPublic();
+        PrivateKey privateKey = keys.getPrivate();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new GenerateKeyApp();
     }
-    
 }
