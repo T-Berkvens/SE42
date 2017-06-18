51b5eb96b35a49d27d06cc20a9b6c71f4f58ae2d
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-18 20:15:34 +0200

    added controls

--- OpdrachtTwee/src/opdrachttwee/HomeController.java
+++ OpdrachtTwee/src/opdrachttwee/HomeController.java
@@ -11,6 +11,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -18,7 +20,11 @@ import javafx.fxml.Initializable;
  * @author Arno
  */
 public class HomeController implements Initializable {
-
+    @FXML
+    TextField txtPassword;
+    @FXML
+    TextArea txtContent;
+    
     /**
      * Initializes the controller class.
      */
