335aa2e6ab6f22d59536ebfb7f229af6db6c02ea
Twan Berkvens <twan_Berkvens@live.nl>
2017-06-18 20:15:37 +0200

    generateslate

---
+++ OpdrachtTwee/src/opdrachttwee/HomeController.java
@@ -0,0 +1,48 @@
+package opdrachttwee;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.security.SecureRandom;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Arno
+ */
+public class HomeController implements Initializable {
+
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+    @FXML
+    private void btnEncrypt(ActionEvent event){
+        
+    }
+    
+    @FXML
+    private void btnDecrypt(ActionEvent event){
+        
+    }
+    
+    private byte[] generateSalt()
+    {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[20];
+        random.nextBytes(bytes);
+        return bytes;
+    }
+}
