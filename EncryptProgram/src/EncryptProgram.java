5f9adc43ff02ab4003d83c27e57d7a27f055f784
Dekkers Los,Arno A.J.P <a.dekkerslos@student.fontys.nl>
2017-06-18 16:21:21 +0200

    updated git ignore tried encrypting

---
+++ EncryptProgram/src/EncryptProgram.java
@@ -0,0 +1,62 @@
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import javax.crypto.Cipher;
+/**
+ *
+ * @author Arno
+ */
+public class EncryptProgram {
+    private PrivateKey privateKey;
+    private byte[] input;
+    
+    public EncryptProgram(String author){
+        try{
+            retrievePrivateKey();
+            getInputFile();
+            encryptWithPrivate(author);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    
+    public void retrievePrivateKey()throws Exception{
+        byte[] keyBytes = Files.readAllBytes(new File("../privateKey").toPath());
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        privateKey = kf.generatePrivate(spec);
+    }
+    
+    public void getInputFile()throws Exception{
+        input = Files.readAllBytes(new File("../input.txt").toPath());
+    }
+    
+    public void encryptWithPrivate(String author)throws Exception{
+        File output = new File("../INPUT(SignedBy" + author + ").EXT");
+        Cipher cipher = Cipher.getInstance("RSA");;
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        writeToFile(output, cipher.doFinal(input));
+    }
+    
+    public void writeToFile(File output, byte[] toWrite)throws Exception{
+        FileOutputStream fos = new FileOutputStream(output);
+        fos.write(toWrite);
+        fos.flush();
+        fos.close();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter the name of who is encrytping the file:");
+        String encryptedBy = "Bert";
+                //System.console().readLine();
+        new EncryptProgram(encryptedBy);
+        // TODO code application logic here
+    }
+    
+}
