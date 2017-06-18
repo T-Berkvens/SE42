64f5df5c350bd497ed318a59a8ff94136c4e3b77
Twan Berkvens <twan_Berkvens@live.nl>
2017-06-18 21:16:31 +0200

    klaar

--- OpdrachtTwee/src/opdrachttwee/HomeController.java
+++ OpdrachtTwee/src/opdrachttwee/HomeController.java
@@ -10,8 +10,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,6 +27,13 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * FXML Controller class
@@ -31,6 +45,9 @@ public class HomeController implements Initializable {
     TextField txtPassword;
     @FXML
     TextArea txtContent;
+    
+    byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    IvParameterSpec ivspec = new IvParameterSpec(iv);
     /**
      * Initializes the controller class.
      */
@@ -39,13 +56,60 @@ public class HomeController implements Initializable {
     }    
     
     @FXML
-    private void btnEncrypt(ActionEvent event){
-        
+    private void btnEncrypt(ActionEvent event) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException, InvalidAlgorithmParameterException{
+        char[] password = txtPassword.getText().toCharArray();
+        String text = txtContent.getText();
+        txtPassword.setText("");
+        txtContent.setText("");
+        byte[] salt = generateSalt();
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Key key = (Key)getKey(password, salt);
+        cipher.init(Cipher.ENCRYPT_MODE, key, ivspec);
+        byte[] ciphertext = cipher.doFinal(text.getBytes("UTF-8"));
+        File f = new File("../text");
+        writeToFile(f, ciphertext, salt);
+        txtContent.setText(new String(ciphertext));
+        resetPassword(password);
+    }
+    
+    private void resetPassword(char[] password)
+    {
+        for (int i = 0; i < password.length; i++) {
+            password[i] = '0';
+        }
+    }
+    
+    private SecretKey getKey(char[] password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password, salt, 935, 128);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKey secret = new SecretKeySpec(tmp.getEncoded(), "AES");
+        return secret;
     }
     
     @FXML
-    private void btnDecrypt(ActionEvent event){
-        
+    private void btnDecrypt(ActionEvent event) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException{
+        char[] password = txtPassword.getText().toCharArray();
+        txtPassword.setText("");
+        File f = new File("../text");
+        byte[] totalText = getFileInBytes(f);
+        byte[] salt = new byte[20];
+        for (int i = 0; i < 20; i++)
+        {
+            salt[i] = totalText[i];
+        }
+        byte[] text = new byte[totalText.length - 20];
+        for (int i = 20; i < totalText.length; i++)
+        {
+            text[i-20] = totalText[i];
+        }
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        Key key = (Key)getKey(password, salt);
+        cipher.init(Cipher.DECRYPT_MODE, key, ivspec);
+        byte[] ciphertext = cipher.doFinal(text);
+        txtContent.setText(new String(ciphertext));
+        resetPassword(password);
     }
     
     private byte[] generateSalt()
@@ -64,8 +128,9 @@ public class HomeController implements Initializable {
         return fbytes;
     }
     
-    private void writeToFile(File output, byte[] toWrite) throws IllegalBlockSizeException, BadPaddingException, IOException {
+    private void writeToFile(File output, byte[] toWrite, byte[] salt) throws IllegalBlockSizeException, BadPaddingException, IOException {
         FileOutputStream fos = new FileOutputStream(output);
+        fos.write(salt);
         fos.write(toWrite);
         fos.flush();
         fos.close();
